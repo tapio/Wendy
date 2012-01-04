4a3b43bbe5fe380cdb96eb8e713ff1f671f773e2
Tapio Vierros <tapiovierros@gmail.com>
2012-01-04 16:39:48 +0200

    Test defines by unifying DeferredMasked*.
    Sponza materials also converted.

--- media/wendy/DeferredMapped.fs
+++ media/wendy/DeferredMapped.fs
@@ -5,9 +5,24 @@ uniform sampler2D normalmap;
 varying vec3 normal;
 varying vec2 texCoord;
 
+#ifdef SPECULAR_MASK
+  #define MASK texture2D(normalmap, texCoord).a
+#else
+  #define MASK 0.0
+#endif
+
 void main()
 {
-  gl_FragData[0] = texture2D(colormap, texCoord);
-  gl_FragData[1] = vec4(normalize(normal) / 2.0 + 0.5, 0.0);
+#ifdef MASKED
+  vec4 color = texture2D(colormap, texCoord);
+  if (color.a - 0.5 < 0.0)
+    discard;
+#else
+  #define color texture2D(colormap, texCoord)
+#endif
+
+  gl_FragData[0] = color;
+
+  gl_FragData[1] = vec4(normalize(normal) / 2.0 + 0.5, MASK);
 }
 
